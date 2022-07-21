8dbc7f6f3c5405f8eeb2c41e6a2672b21c55f05f
leejieun <lgeun123@naver.com>
2022-07-21 09:31:18 +0900

    [B] ADD_meetingRoom&Oauth entity

--- backend/src/main/java/com/sos/owo/domain/MeetingOauth.java
+++ backend/src/main/java/com/sos/owo/domain/MeetingOauth.java
@@ -1,4 +1,26 @@
 package com.sos.owo.domain;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter @Setter
+@Table(name = "meeting_oauth")
 public class MeetingOauth {
+
+    @Id @GeneratedValue
+    @Column(name = "meeting_oauth_id")
+    private int id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "meeting_room_id")
+    private MeetingRoom meetingRoomId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member memberId;
+
+    private int isOauth;
 }
