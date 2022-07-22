df90c9bc8f4c9e4cf97939668ec85f7a666e590a
나장엽 <kaydenna92@gmail.com>
2022-07-22 10:14:21 +0900

    [F] UPDATE_Carousel.vue

---
+++ frontend/owo/src/components/MainPage/TodayKing.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="todayKing">
+  <h1>오늘의 운동왕은 누구~? </h1>
+  </div>
+</template>
+
+<script>
+export default {
+};
+</script>
+
+<style>
+.todayKing {
+  margin: 50px;
+  margin-left: 10px;
+  text-align: left;
+}
+</style>
