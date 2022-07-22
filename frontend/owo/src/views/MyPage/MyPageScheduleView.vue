9dc25f175cabc5086a1d40bdf7fe3c37e2e048c9
mooyah5 <qorgkssk123@gmail.com>
2022-07-22 18:38:20 +0900

    [F] ADD_mypage_update

--- frontend/owo/src/views/MyPage/MyPageScheduleView.vue
+++ frontend/owo/src/views/MyPage/MyPageScheduleView.vue
@@ -3,24 +3,38 @@
     <div class="title">
       <h3>{{ user.nick }}님의 캘린더</h3>
     </div>
+    <div class="calendar">
+      <MyCalendar/>
+    </div>
+  </div>
+  <div>
+    <p>안녕</p>
   </div>
 </template>
+
 <script>
+import MyCalendar from '@/components/MyPage/MyCalendar.vue';
+
 export default {
-  name: 'MyPageScheduleView',
-  components: {},
+  components: { MyCalendar },
   data() {
     return {
       user: {
         nick: '한나',
       },
+      value: '',
     };
   },
   setup() {},
   created() {},
   mounted() {},
   unmounted() {},
-  methods: {},
+  methods: {
+    dateClass(ymd, date) {
+      const day = date.getDate();
+      return day >= 10 && day <= 19 ? 'table-info' : '';
+    },
+  },
 };
 </script>
 
