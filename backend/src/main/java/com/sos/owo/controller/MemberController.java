8fad5e038508ed14a1f6207249cef56a2667e492
daseul <dew4325@naver.com>
2022-07-22 16:06:38 +0900

    [B]ADD_경험치 저장
    #S07P12C202-521 경쟁/영상/자유 경험치 저장

--- backend/src/main/java/com/sos/owo/controller/MemberController.java
+++ backend/src/main/java/com/sos/owo/controller/MemberController.java
@@ -6,9 +6,11 @@ import com.sos.owo.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Stream;
 
 @RestController
 @RequiredArgsConstructor
@@ -29,6 +31,50 @@ public class MemberController {
         }
 
     }
+
+
+    @GetMapping("/api/user/compete/{memberId}")
+    public ResponseEntity<?> findBestScore(@PathVariable("memberId") int memberId, Model model){
+        try {
+            List<Integer> bestScore = memberService.findBestScore(memberId);
+            model.addAttribute("bestScores", bestScore);
+            return new ResponseEntity<List<Integer>>( memberService.findBestScore(memberId), HttpStatus.OK);
+        } catch (IllegalStateException e){
+            return new ResponseEntity<String>("OVERLAP", HttpStatus.CONFLICT);
+        } catch (Exception e){
+            e.printStackTrace();
+            return new ResponseEntity<String>("SERVER_ERROR", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+    }
+
+    @PutMapping("/api/user/point/{point}/{memberId}")
+    public ResponseEntity<?> savePoint(@PathVariable("point") int point, @PathVariable("memberId") int memberId){
+        try {
+            memberService.savePoint(point, memberId);
+            return new ResponseEntity<String>("경쟁모드 경험치 저장성공", HttpStatus.OK);
+        } catch (IllegalStateException e){
+            return new ResponseEntity<String>("OVERLAP", HttpStatus.CONFLICT);
+        } catch (Exception e){
+            e.printStackTrace();
+            return new ResponseEntity<String>("SERVER_ERROR", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PutMapping("/api/user/point/{exp}/{memberId}")
+    public ResponseEntity<?> saveExp(@PathVariable("exp") int exp, @PathVariable("memberId") int memberId){
+        try {
+            memberService.saveExp(exp, memberId);
+            return new ResponseEntity<String>("자유/영상모드 경험치 저장성공", HttpStatus.OK);
+        } catch (IllegalStateException e){
+            return new ResponseEntity<String>("OVERLAP", HttpStatus.CONFLICT);
+        } catch (Exception e){
+            e.printStackTrace();
+            return new ResponseEntity<String>("SERVER_ERROR", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+
 
 
 }
--- backend/src/main/java/com/sos/owo/service/MemberService.java
+++ backend/src/main/java/com/sos/owo/service/MemberService.java
@@ -1,6 +1,7 @@
 package com.sos.owo.service;
 
 import com.sos.owo.domain.Member;
+import com.sos.owo.domain.repository.CompeteRepository;
 import com.sos.owo.domain.repository.MemberRepository;
 import com.sos.owo.dto.MemberResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -8,12 +9,16 @@ import org.springframework.stereotype.Service;
 
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Service
 //@Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class MemberService {
 
     private final MemberRepository memberRepository;
+
+    private final CompeteRepository competeRepository;
 
     // 회원가입
     @Transactional
@@ -47,4 +52,20 @@ public class MemberService {
         if(userEmailDuplicate) throw new IllegalStateException("이미 존재하는 회원입니다.");
 
     }
+
+    @Transactional
+    public List<Integer> findBestScore(int memberId){
+        return competeRepository.findBestScore(memberId);
+    }
+
+    @Transactional
+    public void savePoint(int point, int id){
+        competeRepository.savePoint(point, id);
+    }
+
+    @Transactional
+    public void saveExp(int exp, int id){
+        memberRepository.saveExp(exp, id);
+    }
+
 }
