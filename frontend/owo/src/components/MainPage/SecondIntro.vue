2fc9bacf243743ec85ab5be5f0cc290df0730306
mooyah5 <qorgkssk123@gmail.com>
2022-07-25 16:42:56 +0900

    [F] ADD_mainpage_composition

---
+++ frontend/owo/src/components/MainPage/SecondIntro.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="second-intro">
+
+  </div>
+</template>
+<script>
+export default {
+  name: 'SecondIntro',
+  components: {},
+  data() {
+    return {
+      a: '',
+    };
+  },
+  setup() {},
+  created() {},
+  moundted() {},
+  unmounted() {},
+  methods: {},
+};
+</script>
+
+<style scoped>
+.second-intro {
+  height: 100vh;
+  background-color: rgb(75, 74, 89);
+}
+</style>
