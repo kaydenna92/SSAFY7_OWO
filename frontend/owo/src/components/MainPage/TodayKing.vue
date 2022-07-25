11238bcfbcccee6034d8cf5d4daf55d491c83719
mooyah5 <qorgkssk123@gmail.com>
2022-07-25 17:06:50 +0900

    [F] ETC_Mainpage..

--- frontend/owo/src/components/MainPage/TodayKing.vue
+++ frontend/owo/src/components/MainPage/TodayKing.vue
@@ -2,15 +2,19 @@
   <div class="todayKing">
     <h1 class="lg-title text-center my-5">오늘의 운동왕은 누구~?</h1>
     <div class="wrapper d-flex justify-content-evenly">
+
       <div class="my-exercise-analy">
         <h5 class="sm-title">나의 운동 분석</h5>
         <div>
 
         </div>
       </div>
+
       <div class="king-record">
         <h5 class="sm-title">"{{king}}"의 Record</h5>
+
       </div>
+
       <div class="week-rank">
         <h5 class="sm-title">{{month}}월 {{weekNum}}째주 운동왕</h5>
       </div>
