e78ec64a2284591a9d0b0226742fa9074694ddec
이승희 <hithere1012@naver.com>
2022-07-28 01:33:53 +0900

    [B] ADD_member controller api swagger
    #S07P12C202-550
    Member Controller내에 있는 API에 스웨거처리를 하였습니다.

--- backend/src/main/java/com/sos/owo/dto/MemberSaveRequestDto.java
+++ backend/src/main/java/com/sos/owo/dto/MemberSaveRequestDto.java
@@ -1,6 +1,8 @@
 package com.sos.owo.dto;
 
 import com.sos.owo.domain.Member;
+import io.swagger.annotations.ApiModelProperty;
+import io.swagger.annotations.ApiParam;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -8,7 +10,11 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 public class MemberSaveRequestDto {
+    @ApiModelProperty(example="hithere1012@naver.com")
+    @ApiParam(value = "사용자 email", type = "String")
     private String email;
+    @ApiModelProperty(example="1234")
+    @ApiParam(value = "사용자 password", type = "String")
     private String password;
 
     @Builder
