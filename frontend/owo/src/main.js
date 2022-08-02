a98b9d7bdfe9ed84548a9ede387ed91dc80f938a
kaydenna92 <61959111+kaydennna92@users.noreply.github.com>
2022-08-02 10:56:22 +0900

    [F] ADD VUEX_Modules

--- frontend/owo/src/main.js
+++ frontend/owo/src/main.js
@@ -4,7 +4,6 @@ import VCalendar from 'v-calendar';
 import App from './App.vue';
 import router from './router';
 import store from './store';
-// import BootstrapVue3 from 'bootstrap-vue-3';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue-3/dist/bootstrap-vue-3.css';
 import 'v-calendar/dist/style.css';
