01f28bd8dd8f9644e955e9782580ddf49f67fff3
이준호 <nak3652@naver.com>
2022-08-03 15:52:41 +0900

    [F] CH_roomusername

--- frontend/owo/src/components/Room/WebRTCYoutube.vue
+++ frontend/owo/src/components/Room/WebRTCYoutube.vue
@@ -1,23 +1,27 @@
 <template>
-    <div class="mx-1">
-        <div class="card ms-2 me-2 mb-2 d-flex justify-content-center align-items-center"
-        style="width: 22rem; height:14rem;">
-        <div>
-          <button id="img" @click="get_out"><img style="width:25px;"
-          src="@/assets/icon/get_out.png" alt="">
-          </button>
-        </div>
-        <div>
-            <p>개인 WebRTC 영상(Youtube)</p>
-        </div>
-        </div>
+  <div class="card mx-2 my-2" style="width: 22rem; height:14rem;">
+    <div class="ms-2 my-2" style="text-align:left;">
+      <span class="p-1 text-white" style="background-color:#4e8aff;">{{ username }}</span>
     </div>
+    <div class="ms-2 me-2 mb-2 d-flex justify-content-center align-items-center"
+    style="width: 100%; height:100%;">
+      <div>
+        <button id="img" @click="get_out"><img style="width:25px;"
+        src="@/assets/icon/get_out.png" alt="">
+        </button>
+      </div>
+      <div>
+          <p>개인 WebRTC 영상(Youtube)</p>
+      </div>
+    </div>
+  </div>
 </template>
 <script>
 export default {
   components: {},
   data() {
     return {
+      username: '이준호',
       user_isActive: false,
       user_get_out: false,
     };
@@ -29,9 +33,6 @@ export default {
   methods: {
     get_out() {
       alert('추방하시겠습니까?');
-      // alert 대신 confirm으로 별경한다.
-      // const getout = confirm('추방하시겠습니까?')
-      // comfirm 받은 것을 넘겨서 추방할 수 있도록 한다.
     },
   },
 };
--- frontend/owo/src/views/Room/CompetitionView.vue
+++ frontend/owo/src/views/Room/CompetitionView.vue
@@ -1,39 +1,41 @@
 <template>
   <div class="container-wrap d-flex justify-content-center">
-      <div>
-        <div class="d-flex justify-content-between align-items-center"
-        style="height:80px;">
-          <h3 class="m-0">#ROUND 1 : 런지</h3>
-          <Timer/>
-        </div>
-        <div class="container d-flex align-items-start justify-content-between">
-          <div class="row">
-            <WebRTC v-if="true" class="col-lg-4 mb-5"></WebRTC>
-            <WebRTC v-if="true" class="col-4 mb-5"></WebRTC>
-            <WebRTC v-if="true" class="col-4 mb-5"></WebRTC>
-            <WebRTC v-if="true" class="col-4 mb-5"></WebRTC>
-            <WebRTC v-if="true" class="col-4 mb-5"></WebRTC>
-            <WebRTC v-if="true" class="col-4 mb-5"></WebRTC>
-            <RoomButton></RoomButton>
-          </div>
+    <div>
+      <div class="d-flex justify-content-between align-items-center"
+      style="height:80px;">
+        <h3 class="m-0">#ROUND 1 : 런지</h3>
+        <Timer/>
+      </div>
+      <div class="container d-flex align-items-start justify-content-between">
+        <div class="row">
+          <WebRTCCompetition v-if="true" class="col-lg-4 mb-5"></WebRTCCompetition>
+          <WebRTCCompetition v-if="true" class="col-lg-4 mb-5"></WebRTCCompetition>
+          <WebRTCCompetition v-if="true" class="col-lg-4 mb-5"></WebRTCCompetition>
+          <WebRTCCompetition v-if="true" class="col-lg-4 mb-5"></WebRTCCompetition>
+          <WebRTCCompetition v-if="true" class="col-lg-4 mb-5"></WebRTCCompetition>
+          <WebRTCCompetition v-if="true" class="col-lg-4 mb-5"></WebRTCCompetition>
+          <RoomButton></RoomButton>
         </div>
       </div>
     </div>
+  </div>
 </template>
 <script>
 import Timer from '@/components/SetTimer.vue';
-import WebRTC from '@/components/Room/WebRTC.vue';
+import WebRTCCompetition from '@/components/Room/WebRTCCompetition.vue';
 import RoomButton from '@/components/Room/RoomButton.vue';
 
 export default {
   name: 'CompetitionView',
   components: {
     Timer,
-    WebRTC,
+    WebRTCCompetition,
     RoomButton,
   },
   data() {
     return {
+      exercise: {
+      },
     };
   },
   setup() {},
