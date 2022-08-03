57a4b2e39b11f93da152c6745beb030f528b57a3
kaydenna92 <61959111+kaydennna92@users.noreply.github.com>
2022-08-03 11:57:22 +0900

    [F] UPDATE_store
    Fixed the problem of being deleted on refresh use persistedState

--- frontend/owo/src/store/index.js
+++ frontend/owo/src/store/index.js
@@ -1,8 +1,12 @@
 import { createStore } from 'vuex';
 import { accounts } from '@/store/modules/accounts';
+import createPersistedState from 'vuex-persistedstate';
 
 export default createStore({
   modules: {
     accounts,
   },
+  plugins: [createPersistedState({
+    paths: ['accounts'],
+  })],
 });
