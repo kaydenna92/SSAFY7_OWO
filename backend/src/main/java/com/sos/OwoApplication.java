8756e51fb9671c7b1e23a771c36a584d7dff3c0b
이승희 <hithere1012@naver.com>
2022-08-07 23:52:02 +0900

    Revert "Merge branch 'feature/addTest' into 'main'"
    This reverts merge request !157

--- backend/src/main/java/com/sos/OwoApplication.java
+++ backend/src/main/java/com/sos/OwoApplication.java
@@ -3,13 +3,11 @@ package com.sos;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication
 public class OwoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OwoApplication.class, args);
-
 	}
 
 }
--- backend/src/main/java/com/sos/owo/domain/repository/ProfileImgRepository.java
+++
@@ -1,7 +0,0 @@
-package com.sos.owo.domain.repository;
-
-import com.sos.owo.domain.ProfileImg;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProfileImgRepository extends JpaRepository<ProfileImg, Integer> {
-}
