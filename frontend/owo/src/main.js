983ac7d2be22d259054a42c6db20134e288ef5e1
kaydenna92 <61959111+kaydennna92@users.noreply.github.com>
2022-08-08 00:21:30 +0900

    [F] FIX_error

--- frontend/owo/src/main.js
+++ frontend/owo/src/main.js
@@ -1,13 +1,13 @@
 import { createApp } from 'vue';
 import BootstrapVue3 from 'bootstrap-vue-3';
 import VCalendar from 'v-calendar';
+import VueSweetalert2 from 'vue-sweetalert2';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue-3/dist/bootstrap-vue-3.css';
 import 'v-calendar/dist/style.css';
-import VueSweetalert2 from 'vue-sweetalert2';
 import 'sweetalert2/dist/sweetalert2.min.css';
 
 const app = createApp(App);
