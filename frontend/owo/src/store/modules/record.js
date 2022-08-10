3af1331a62f41d4815b5439666deaceb2ac83027
mooyah5 <qorgkssk123@gmail.com>
2022-08-10 18:31:58 +0900

    [F] FIX

--- frontend/owo/src/store/modules/record.js
+++ frontend/owo/src/store/modules/record.js
@@ -94,16 +94,16 @@ export const record = {
         console.log(err.toJSON());
       });
     // eslint-disable-next-line
-    sendRecord({}, { credentials, credentialsUser }) {
-      console.log(credentialsUser);
-      console.log('------');
-      axios.post(`https://i7c202.p.ssafy.io:8282/api/record/finish/${credentialsUser.memberId}/${credentialsUser.meetingRoomId}`, credentials)
-        .then((res) => {
-          console.log('성공', res.data);
-        })
-        .catch((err) => {
-          console.log('실패', err);
-        });
-    },
+    // sendRecord({}, { credentials, credentialsUser }) {
+    //   console.log(credentialsUser);
+    //   console.log('------');
+    //   axios.post(`https://i7c202.p.ssafy.io:8282/api/record/finish/${credentialsUser.memberId}/${credentialsUser.meetingRoomId}`, credentials)
+    //     .then((res) => {
+    //       console.log('성공', res.data);
+    //     })
+    //     .catch((err) => {
+    //       console.log('실패', err);
+    //     });
+    // },
   },
 };
